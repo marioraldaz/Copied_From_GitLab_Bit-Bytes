8b867adb601aa966f4ad28d695a8b858e6e8a47e
marioraldaz <marioraldaz@gmail.com>
2023-12-17 13:36:32 +0100

    Fixxed nav navigation

--- src/lib/header/NavElem.svelte
+++ src/lib/header/NavElem.svelte
@@ -21,7 +21,7 @@
 	});
 </script>
 
-<a href={`/products/${name}`} on:click={onClick}>
+<a href={`/products/${name}`}>
 	<div class="elem-container">
 		<img src={logo} id={name} class="image" alt="logo-nav-elem" />
 		<span class="elem-name">{name}</span>
