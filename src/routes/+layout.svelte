06a51e7e34eeb41e8021660bcc8d0ed4e9d5085f
marioraldaz <marioraldaz@gmail.com>
2023-12-17 14:44:19 +0100

    added styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,16 @@
 	import Footer from '/src/lib/footer/Footer.svelte';
 </script>
 
-<div class="header"><Header /></div>
-<div class="body"><slot /></div>
-<div class="footer"><Footer /></div>
+<div class="app">
+	<div class="header"><Header /></div>
+	<div class="body"><slot /></div>
+	<div class="footer"><Footer /></div>
+</div>
 
 <style>
+	.app {
+		display: block;
+	}
 	.body {
 		height: fit-content;
 	}
--- src/stores/menus.js
+++ src/stores/menus.js
@@ -5,13 +5,13 @@ var MotherBoards = {
   name: "Motherboards",
   logo: "src/stores/images/MotherBoard.png",
 };
-var CPUs = { name: "CPUs", logo: "src/images/CPU.png" };
-var SSDs = { name: "SSDs", logo: "src/images/SSD.png" };
-var GPUs = { name: "GPUs", logo: "src/images/GPU.png" };
+var CPUs = { name: "CPUs", logo: "/src/images/CPU.png" };
+var SSDs = { name: "SSDs", logo: "/src/images/SSD.png" };
+var GPUs = { name: "GPUs", logo: "/src/images/GPU.png" };
 var Body = { name: "Body" };
 var PC_Creator = {
   name: "PC Creator",
-  logo: "src/images/PC_Creator_logo.png",
+  logo: "/src/images/PC_Creator_logo.png",
 };
 const menus = [Body, RAMs, MotherBoards, CPUs, SSDs, GPUs, PC_Creator];
 const active = "Body";
