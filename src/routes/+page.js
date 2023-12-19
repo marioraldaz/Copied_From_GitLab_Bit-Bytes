53823caa60789e221543ec32cecc9705e4bdf2b1
marioraldaz <marioraldaz@gmail.com>
2023-12-19 10:11:27 +0100

    json-server static images

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,4 +1,5 @@
-export const load = async ()=> {
+export const load = async (loadEvent)=> {
+    const { fetch } = loadEvent;
     const response = await fetch('http://localhost:4000/products');
     const products = await response.json();
     return {products};
