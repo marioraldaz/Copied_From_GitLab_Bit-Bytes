e7c8f7e930261c5e79e5c9e738f80523ad9d79e3
marioraldaz <marioraldaz@gmail.com>
2023-12-25 19:58:39 +0100

    Developing MyProfile svelte component

---
+++ src/lib/header/profile/MyProfile.svelte
@@ -0,0 +1,62 @@
+<script>
+    
+</script>
+
+<div class="MyProfile">
+    <img src="src" alt="MyProfile" class="MyProfile__image" />
+    <h1 class="MyProfile__name">Nombre</h1>
+    <h2 class="MyProfile__email">Email</h2>
+    <a href="/profile/changePassword" class="MyProfile__changePassword">Cambiar contraseña</a>
+    <div class="MyProfile__Address"></div>
+    <div class="MyProfile__PC_Configs"></div>
+    <div class="MyProfile__savedProducts"></div>
+    <div class="MyProfile__boughtProducts"></div>
+    <div class="MyProfile__stats"></div>
+    <div class="MyProfile__comments"></div>
+    <div class="MyProfile__returns"></div>
+</div>
+
+
+<style lang="scss">
+    .MyProfile{
+        background-color: white;
+        &__image{
+            width:5rem;
+            height: 5rem;
+        }
+        &__name{
+            font-size:3rem;
+        }
+        &__email{
+            font-size: 2rem;
+        }
+        &__changePassword{
+            width: 40%;
+            background-color: black;
+        }
+        &__Address{
+            width: 80%;
+        }
+
+        &__PC_Configs{
+
+        }
+        &__savedroducts{
+
+        }
+        &__boughtproducts{
+
+        }
+        &__stats{
+
+        }
+
+        &__comments{
+
+        }
+        &__returns{
+            
+        }
+
+    }
+</style>
